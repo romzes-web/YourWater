ff478b020b1937955d7f76ae89167915379261ba
Roman Ildjanov <67704125+romzes-web@users.noreply.github.com>
2023-01-13 14:56:39 +0300

    HeaderTest edit

--- src/components/__tests__/HeaderTest.spec.js
+++ src/components/__tests__/HeaderTest.spec.js
@@ -5,33 +5,16 @@ import Header from '../Header.vue'
 
 describe('Header', () => {
   it('category 1', () => {
-    const wrapper = mount(Header, { links: { title: 'Главная' } })
+    const wrapper = mount(Header, { 
+      links: { 
+        title: String
+      } 
+    })
     expect(wrapper.text()).toContain('Главная')
+    expect(wrapper.text()).toContain('Каталог')
+    expect(wrapper.text()).toContain('О воде')
+    expect(wrapper.text()).toContain('О доставке')
+    expect(wrapper.text()).toContain('Оплата')
+    expect(wrapper.text()).toContain('Контакты')
   })
-  
-  it('category 2', () => {
-      const wrapper = mount(Header, { links: { title: 'Каталог' } })
-      expect(wrapper.text()).toContain('Каталог')
-    })
-
-  it('category 3', () => {
-      const wrapper = mount(Header, { links: { title: 'О воде' } })
-      expect(wrapper.text()).toContain('О воде')
-    })
-
-  it('category 4', () => {
-      const wrapper = mount(Header, { links: { title: 'О доставке' } })
-      expect(wrapper.text()).toContain('О доставке')
-    })
-
-  it('category 5', () => {
-      const wrapper = mount(Header, { links: { title: 'Оплата' } })
-      expect(wrapper.text()).toContain('Оплата')
-    })
-
-  it('category 6', () => {
-      const wrapper = mount(Header, { links: { title: 'Контакты' } })
-      expect(wrapper.text()).toContain('Контакты')
-    })
-  
 })
